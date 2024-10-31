fe009d3f33b138d7627503b12810436f16960a77
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-10-31 11:27:38 +0530

    host variable

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+require("dotenv").config();
 const http = require("http");
 const express = require("express");
 const { createWSS } = require("./ws");
 const path = require("path");
 
 const app = express();
+const HOST = process.env.HOST;
 
 app.use(express.static(path.join(process.cwd(), "..", "client/dist")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(process.cwd(), "..", "client/dist/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "..", "client/dist/index.html"));
 });
 
 function main() {
   const server = http.createServer(app);
   createWSS(server);
 
-  server.listen(3000, "172.17.4.129", () => {
+  server.listen(3000, HOST, () => {
     console.log("http://localhost:3000");
   });
 }
